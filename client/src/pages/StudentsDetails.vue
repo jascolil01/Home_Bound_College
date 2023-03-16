6b9fb031cdb89809a84c24a187ad2d4fa09ef9a3
Eduardo Puerta <puerta.eduardo10@gmail.com>
2023-03-16 12:36:55 -0400

    changes

--- client/src/pages/StudentsDetails.vue
+++ client/src/pages/StudentsDetails.vue
@@ -1,36 +1,15 @@
 <template>
-  <div>
-
-  </div>
-
-
+  <div></div>
 </template>
 
-
 <script>
-
-
-
 export default {
   name: 'StudentDetails',
-  components: {
-
-  },
-  data: () => ({
-
-  }),
-  mounted () {
-
-  },
-  methods: {
-    
-  }
+  components: {},
+  data: () => ({}),
+  mounted() {},
+  methods: {}
 }
-
 </script>
 
-
-<style>
-
-
-</style>
+<style></style>
