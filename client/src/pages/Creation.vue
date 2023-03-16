b5e70fa1d0d22f4cd64c41f25f8f1dec9962d54d
jascolil01 <jayasco246@gmail.com>
2023-03-16 14:43:13 -0400

    added work to creation page

---
+++ client/src/pages/Creation.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-if="addStudent">
+    <AddStudents />
+  </div>
+
+  <div v-if="addCourse">
+    <MakeCourse />
+  </div>
+</template>
+
+<script>
+import MakeCourse from '@/components/MakeCourse.vue';
+import AddStudents from '../components/AddStudents.vue';
+
+export default {
+  name: 'Creation',
+  components: {
+    MakeCourse,
+    AddStudents
+  },
+  data: () => ({
+    addStudent: false,
+    addCourse: false
+  })
+}
+</script>
