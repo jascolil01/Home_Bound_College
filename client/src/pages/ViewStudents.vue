b248b32c11700707697cd08428e25a78034195ab
Eduardo Puerta <puerta.eduardo10@gmail.com>
2023-03-16 14:38:57 -0400

    added mapping in viewstudents

--- client/src/pages/ViewStudents.vue
+++ client/src/pages/ViewStudents.vue
@@ -1,27 +1,27 @@
 <template>
-  <div
-    classname="student-grid"
-    v-for="student in students"
-    :key="student.id"
-  ></div>
+  <div classname="student-grid">
+    <h3>{{ students.name }}</h3>
+  </div>
 </template>
 
 <script>
+// import { BASE_URL } from '../globals'
+// import axios from 'axios'
 export default {
   name: 'ViewStudents',
   components: {},
   props: {
     students: {}
   },
-  data: () => ({
-    students: []
-  }),
-  mounted() {
-    this.getStudentsById()
-  },
-  methods: {
-    async getStudentsById() {}
-  }
+  // data: () => ({
+  //   students: []
+  // }),
+  mounted() {}
+  // methods: {
+  //   async getStudents() {
+  //     const res = await axios.get(`${BASE_URL}students`)
+  //     res.data
+  //   }
 }
 </script>
 
