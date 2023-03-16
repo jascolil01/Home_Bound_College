2372f1d8f444e28ad91dc9b4c14b7d63e0fcd949
jascolil01 <jayasco246@gmail.com>
2023-03-16 11:00:17 -0400

    finished route.js file

--- client/src/router.js
+++ client/src/router.js
@@ -4,3 +4,20 @@ import StudentsDetails from './components/StudentsDetails'
 import ReportCard from './components/ReportCard'
 import MakeCourse from './components/MakeCourse'
 import AddStudents from './components/AddStudents'
+
+
+const  routes = [
+  {path:'/',component: HomePage, name:'HomePage'},
+  {path:'/students',component: ViewStudents, name:'ViewStudents'},
+  {path:'/students/:student_id',component: StudentsDetails, name:'StudentsDetails'},
+  {path:'/students/report_card/:student_id',component: ReportCard, name:'ReportCard'},
+  {path:'/make_course',component: MakeCourse, name:'MakeCourse'},
+  {path:'/add_student',component: AddStudents, name:'AddStudents'}
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
